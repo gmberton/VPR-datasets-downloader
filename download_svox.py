d1f4296326568121bea57c3dda5f8ee87bba541a
gmberton <bertongabri@gmail.com>
2022-10-30 23:55:34 +0100

    Using gdown to download svox, updated link to svox dataset

--- download_svox.py
+++ download_svox.py
@@ -1,11 +1,15 @@
 
 import os
-from os.path import join
-from google_drive_downloader import GoogleDriveDownloader as gdd
+import shutil
 
-datasets_folder = join(os.curdir, "datasets")
-gdd.download_file_from_google_drive(file_id='16iuk8voW65GaywNUQlWAbDt6HZzAJ_t9',
-                                    dest_path=join(datasets_folder, 'svox.zip'),
-                                    unzip=True)
+import gdown
 
-os.remove(join(datasets_folder, 'svox.zip'))
+datasets_folder = os.path.join(os.curdir, "datasets")
+os.makedirs(datasets_folder, exist_ok=True)
+zip_filepath = os.path.join(datasets_folder, 'svox.zip')
+gdown.download(id="16iuk8voW65GaywNUQlWAbDt6HZzAJ_t9",
+               output=zip_filepath,
+               quiet=False)
+
+shutil.unpack_archive(zip_filepath, datasets_folder)
+os.remove(zip_filepath)
